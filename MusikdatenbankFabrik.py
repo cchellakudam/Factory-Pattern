bf22894fccebdeecf088bee93b9f818773a06d4f
cchellakudam <cchellakudam@student.tgm.ac.at>
2017-05-07 23:36:32 +0200

    erweiterung

--- MusikdatenbankFabrik.py
+++ MusikdatenbankFabrik.py
@@ -8,15 +8,18 @@ class MusikdatenbankFabrik(metaclass=ABCMeta):
 
     def __init__(self):
         self.geladen = False
+        self.playlist=[]
+        self.duration=0
 
     @abstractmethod
     def lade_musik(self):
         pass
 
     def abspielen(self):
-        if not self.geladen:
-            self.lade_musik()
-        for song in self.playlist:
-            song.abspielen()
-            time.sleep(1)
-        print('Wir sind am Ende der Playliste angelangt. Auf Wiedersehen!')
+        pass
+       # if not self.geladen:
+           # self.lade_musik()
+       # for song in self.playlist:
+           # song.abspielen()
+           # time.sleep(1)
+       # print('Wir sind am Ende der Playliste angelangt. Auf Wiedersehen!')
--- factory_musikdatenbank.py
+++ factory_musikdatenbank.py
@@ -1,4 +1,15 @@
-class MusikdatenbankMockupFabrik():
-    pass
-class MusikdatenbankFileFabrik():
-    pass
+
+import Product
+from MusikdatenbankFabrik import MusikdatenbankFabrik
+
+from Musikstueck import Musikstueck
+
+
+class MusikdatenbankFileFabrik(MusikdatenbankFabrik, Musikstueck):
+
+    def lade_musik(self):
+        directory = 'C:/Users/Kithu/Desktop/NewFactory/Musik'
+        return directory
+    def abspielen(self):
+      Product.start()
+
